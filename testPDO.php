0e7318a8a8e93785e958fa014404efc5b72d4005
aymen <aymen.sellaouti@gmail.com>
2020-04-30 12:48:58 +0100

    second commit création page de listing des étudiants

--- testPDO.php
+++ testPDO.php
@@ -22,9 +22,39 @@ $etudiants = $response->fetchAll(PDO::FETCH_OBJ);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
     <title>Document</title>
 </head>
 <body>
-
+<div class="alert alert-primary">
+    Liste des étudiants
+</div>
+   <table class="table table-hover">
+       <thead>
+            <th>id</th>
+            <th>name</th>
+            <th>age</th>
+            <th>cin</th>
+       </thead>
+       <tbody>
+       <?php
+            foreach ($etudiants as $etudiant) {
+       ?>
+            <tr>
+                <td><?= $etudiant->id?></td>
+                <td><?= $etudiant->name?></td>
+                <td><?= $etudiant->age?></td>
+                <td><?= $etudiant->cin?></td>
+            </tr>
+       <?php
+            }
+       ?>
+       </tbody>
+   </table>
+<div class="form-group has-success">
+    <label class="form-control-label" for="inputSuccess1">Valid input</label>
+    <input type="text" value="correct value" class="form-control is-valid" id="inputValid">
+    <div class="valid-feedback">Success! You've done it.</div>
+</div>
 </body>
 </html>
