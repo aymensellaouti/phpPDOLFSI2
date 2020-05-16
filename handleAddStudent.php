89578d1b8c1b63e6a2da46a548f80c26aa3c95fa
aymen <aymen.sellaouti@gmail.com>
2020-05-16 11:21:06 +0100

    correct add Bug
    Problem with sql request copy past character encoding

--- handleAddStudent.php
+++ handleAddStudent.php
@@ -2,18 +2,22 @@
 require_once 'authentification/isAuthenticated.php';
 require_once 'BD/getConnextion.php';
 
-if (
-    isset($_POST['name'])
-    && isset($_POST['age'])
-    && isset($_POST['cin'])
-    && isset($_POST['section'])) {
-    $req = "INSERT INTO `etudiant` (`id`, `name`, `age`, `cin`, `section_id`) VALUES (NULL, 'Firas', 24, '1245', 1);";
+if (isset($_POST['name']) && isset($_POST['age']) && isset($_POST['cin']) && isset($_POST['section'])) {
+    $req = "INSERT INTO etudiant (name, age, cin, section_id) VALUES (:name, :age, :cin, :section)";
     $response = $bdd->prepare($req);
     $result = $response->execute(
         array(
+            'name' => $_POST['name'],
+            'age' => $_POST['age'],
+            'cin' => $_POST['cin'],
+            'section' => $_POST['section']
         )
     );
-    die($result);
+    if ($result) {
+        $_SESSION['success'] = "L'étudiant(e) ${_POST['name']} a été ajouté avec succès";
+    } else {
+        $_SESSION['error'] = "Problème avec la base de données";
+    }
     header('location:students.php');
 } else {
     header('location:home.php');
